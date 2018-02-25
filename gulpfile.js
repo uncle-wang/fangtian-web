3ef0d68a674256d1fe8eb5e345204b8c9af1e661
jin.wang <jin.wang@reallytek.com>
2018-02-25 18:20:31 +0800

    初始化

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cleancss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
+var jsSrc = './src/js/*.js';
+var jsDst = './build/js/';
+var lessSrc = './src/less/*.less';
+var cssDst  = './build/css/';
+
+// 编译less文件
+gulp.task('compileLess', function() {
+
+	gulp
+		.src(lessSrc)
+		.pipe(less())
+		.pipe(cleancss())
+		.pipe(gulp.dest(cssDst));
+});
+// 监测less文件变化
+gulp.task('watchless', function() {
+
+	gulp.watch(lessSrc, ['compileLess']);
+});
+
+// 压缩js文件
+gulp.task('uglifyJs', function() {
+
+	gulp
+		.src(jsSrc)
+		.pipe(uglify())
+		.pipe(gulp.dest(jsDst));
+});
+
+// 监测js文件变化
+gulp.task('watchjs', function() {
+
+	gulp.watch(jsSrc, ['uglifyJs']);
+});
+
+gulp.task('watch', ['watchless', 'watchjs']);
+gulp.task('default', ['compileLess', 'uglifyJs']);
