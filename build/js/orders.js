c9abf45b30661748f1d779b7e130b27ae5badf7f
tian.fang <tian.fang@ft.com>
2018-04-14 19:22:21 +0800

    订单历史

--- build/js/orders.js
+++ build/js/orders.js
@@ -31,16 +31,6 @@ var getTypeStr = function(type) {
 	}
 	return arr[type] || '异常';
 };
-var getStatusStr = function(status) {
-
-	var arr = ['等待支付', '已结束', '等待结果'];
-	return arr[parseInt(status)] || '异常';
-};
-var getResultStr = function(result) {
-
-	var arr = ['等待结果', '胜利', '失败'];
-	return arr[parseInt(result)] || '异常';
-};
 var zeroFixed = function(n) {
 
 	if (Number(n) < 10) {
@@ -65,34 +55,43 @@ var initOrderData = function(orderList) {
 		var orderInfo = orderList[i];
 		var orderStatus = orderInfo.status;
 		var orderType = orderInfo.type;
-		var $orderItem = $('<li class="order-item"></li>');
-		var $wrapA = $(getOrderWrap('期号', orderInfo.game_id));
-		var $wrapB = $(getOrderWrap('类型', getTypeStr(orderType)));
-		var $wrapC = $(getOrderWrap('金额', orderInfo.amount));
-		var $wrapD = $(getOrderWrap('状态', getStatusStr(orderStatus)));
-		var $wrapE = $(getOrderWrap('创建时间', formatTime(orderInfo.create_time)));
-		var resultStr, resultClass;
-		// 已结束
+		var $orderItem = $('\
+			<li class="order-item">\
+				<div class="order-left">\
+					<div class="row">\
+						<span class="order-id">' + orderInfo.game_id + '期</span>\
+						<span class="order-time">' + formatTime(orderInfo.create_time) + '</span>\
+					</div>\
+					<div class="row">\
+						<span class="order-type">' + getTypeStr(orderType) + '</span>\
+						<span class="order-quota">' + orderInfo.amount + '豆</span>\
+					</div>\
+				</div>\
+				<div class="order-right"></div>\
+			</li>\
+		');
+		var $orderRight = $orderItem.find('.order-right');
 		if (orderStatus === '1') {
 			var orderResult = orderInfo.result;
 			if (orderResult === orderType) {
-				resultClass = 'won';
-				resultStr = getResultStr(1);
+				$orderRight.html('<div class="won">胜利</div>');
 			}
 			else {
-				resultClass = 'fail';
-				resultStr = getResultStr(2);
+				$orderRight.html('<div class="fail">失败</div>');
 			}
 		}
-		// 未结束
-		else {
-			resultStr = getResultStr(0);
-			resultClass = null;
+		else if (orderStatus === '0') {
+			$orderRight.html('\
+				<div class="order-pay-text">等待支付</div>\
+				<a class="order-btn">支付</a>\
+			');
 		}
-		var $wrapF = getOrderWrap('结果', resultStr, resultClass);
-		$orderItem.append($wrapA).append($wrapB).append($wrapC).append($wrapD).append($wrapE).append($wrapF);
+		else {
+			$orderRight.html('<div class="pending">等待开奖</div>');
+		}
 		$orderList.append($orderItem);
 	}
 };
 
+common.payform.init();
 getOrderHistory();
