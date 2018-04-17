9c534bbacfbac3198a25c07acf27a52b41b8803f
tian.fang <tian.fang@ft.com>
2018-04-17 13:24:45 +0800

    提现记录

---
+++ build/js/pickup.js
@@ -0,0 +1,58 @@
+// 获取充值记录
+var getPickupHistory = function() {
+
+	common.ajax({
+
+		url: '/api/getPickupHistory',
+		success: function(data) {
+			if (data.status === 1000) {
+				initPickupData(data.pickupList);
+			}
+		}
+	});
+};
+
+var zeroFixed = function(n) {
+
+	if (Number(n) < 10) {
+		return '0' + n;
+	}
+	return n;
+};
+var formatTime = function(stamp) {
+
+	var d = new Date(stamp);
+	var year = d.getFullYear().toString().substr(2);
+	var month = zeroFixed(d.getMonth() + 1);
+	var date = zeroFixed(d.getDate());
+	var hour = zeroFixed(d.getHours());
+	var minute = zeroFixed(d.getMinutes());
+	return '' + year + '-' + month + '-' + date + ' ' + hour + ':' + minute;
+};
+var initPickupData = function(pickupList) {
+
+	var $pickupList = $('.pickup-list');
+	for (var i = 0; i < pickupList.length; i ++) {
+		var pickupInfo = pickupList[i];
+		var pickupStatus = pickupInfo.status;
+		var $pickupItem = $('\
+			<li class="pickup-item">\
+				<div class="pickup-left">\
+					<div class="pickup-time">' + formatTime(pickupInfo.create_time) + '</div>\
+					<div class="pickup-amount">' + pickupInfo.quota + '豆</div>\
+				</div>\
+				<div class="pickup-right"></div>\
+			</li>\
+		');
+		var $pickupRight = $pickupItem.find('.pickup-right');
+		if (pickupStatus === '1') {
+			$pickupRight.html('<div class="complete">已完成</div>');
+		}
+		else {
+			$pickupRight.html('<div class="pending">待处理</div>');
+		}
+		$pickupList.append($pickupItem);
+	}
+};
+
+getPickupHistory();
